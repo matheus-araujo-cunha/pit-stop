c40cbf5a02a0eee5b483474388690cd4fc818d7e
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-07-20 17:53:48 -0300

    fix:Add Stock/GenericViews

--- products/views.py
+++ products/views.py
@@ -10,3 +10,9 @@ class ProductsView(generics.ListCreateAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
 
+
+    def perform_create(self, serializer: ProductsSerializer):
+        valid_stock = serializer.validated_data.get("stock")
+        stock = Stock.objects.create(**valid_stock)
+
+        serializer.save(stock=stock)
