0d9cf9ec3576cc6e38350e0096d46cdd9bc12b3e
breno174 <breno.angelim@hotmail.com>
2022-07-22 10:42:24 -0300

    fix: apply migrations

--- stock/serializers.py
+++ stock/serializers.py
@@ -2,4 +2,3 @@ from rest_framework import serializers
 
 class StockSerializer(serializers.Serializer):
     quantity = serializers.FloatField(default=1)
-    categorie = serializers.CharField(max_length=50)
